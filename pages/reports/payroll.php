b636d6305b3bf8e07111095e33396b79e7acad8f
magokmen <beyzade83@gmail.com>
2024-06-10 17:31:06 +0300

    chore: Refactor payroll.php and style.css files
    Refactor the payroll.php file to improve the structure and readability of the HTML code. Add appropriate indentation and line breaks to enhance code organization.
    
    In the style.css file, add a new CSS rule to the .table class to set the border-collapse property to collapse. This change will improve the appearance of the table by removing the spacing between table cells.
    
    Refactor the code to follow best practices and improve maintainability.

--- pages/reports/payroll.php
+++ pages/reports/payroll.php
@@ -19,7 +19,144 @@ $sql->execute(array(143));
 $result = $sql->fetch(PDO::FETCH_OBJ);
 
 
-$html = '<h4>Personel Maas : '.$result->toplam_maas.'</h4>';
+
+
+$html = '<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>' . $document . '</title>
+</head>
+    
+    <style>
+        ' .  file_get_contents("../../src/css/pdfcss.css") . '
+    </style>
+
+<table >
+    <tr>
+        <td colspan="3" class="col-12 form-head">PERSONEL MAAŞ BORDROSU</td>
+        
+    </tr>
+    <tr>
+        <td rowspan="5" class="col-3">LOGO BURAYA GELECEK</td>
+        <td class="col-2">Adı Soyadı :</td>
+        <td class="col-7 text-bold">Mehmet Ali Gökmen</td>
+    </tr>
+    <tr>
+        <td class="col-2">Görevi :</td>
+        <td class="col-7 text-bold">Usta</td>
+    </tr>
+    <tr>
+        <td class="col-2">Ay Günü :</td>
+        <td class="col-7 text-bold">30</td>
+    </tr>
+    <tr>
+        <td class="col-2">Ay / Yıl :</td>
+        <td class="col-7 text-bold">Haziran / 2024</td>
+    </tr>
+    <tr>
+        <td class="col-2">Firma / Proje :</td>
+        <td class="col-7 text-bold">Mbe Yazılım</td>
+    </tr>
+
+    <tr>
+        <td colspan="3" class="col-12 form-head">MAAŞ BİLGİLERİ</td>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <table class="table">
+               
+                <tr>
+                    <td class="col-6 text-center border-right" >KAZANÇLAR</td>
+                    <td class="col-6 text-center ">KESİNTİLER</td>
+                </tr>
+               
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <table class="table">   
+    
+                    <td class="col-6" style="border: 1px solid gray;">
+                        <table class="table">   
+                            <tr style="border-bottom: 1px solid gray;">
+                                <td colspan="4">KAZANÇLAR</td>
+                                <td colspan="2">Gün</td>
+                                <td colspan="3">Saat</td>
+                                <td colspan="3">Tutar</td>
+                            </tr>';
+
+                            
+
+                         $html .= '<tr>
+                                <td colspan="4">Normal Çalışma</td>
+                                <td colspan="2">3 Gün</td>
+                                <td colspan="3">24 Saat</td>
+                                <td colspan="3">3.000,00 TL</td>
+                            </tr>';
+
+                            $html .= '<tr>
+                                <td colspan="4">Fazla Mesai</td>
+                                <td colspan="2">3 Gün</td>
+                                <td colspan="3">24 Saat</td>
+                                <td colspan="3">3.000,00 TL</td>';
+
+
+                       $html .= '</table>
+                    </td>
+                   <td class="col-6" style="border: 1px solid gray;">
+                         <table class="table">   
+                           <tr style="border-bottom: 1px solid gray;">
+                                <td colspan="4">KESİNTİLER</td>
+                                <td colspan="2">Gün</td>
+                                <td colspan="3">Saat</td>
+                                <td colspan="3">Tutar</td>
+                            </tr>';
+                            $html .= '<tr>
+                                        <td colspan="4">Bes Kesintisi</td>
+                                        <td colspan="2"></td>
+                                        <td colspan="3"></td>
+                                        <td colspan="3">900,00 TL</td>
+                                    </tr>';
+
+                        $html .='</table>
+                    </td>
+                 
+               
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3" class="col-12 form-head">ÖDEME BİLGİLERİ</td>
+    </tr>
+    
+    <tr>
+        <td colspan="3">
+            <table class="table">
+                <tr>
+                    <td class="col-4 text-center border-right" >KAZANÇ TOPLAMI</td>
+                    <td class="col-4 text-center border-right" >KESİNTİ TOPLAMI</td>
+                    <td class="col-4 text-center border-right" >ÖDENECEK TUTAR</td>
+                </tr>
+                <tr>
+                    <td class="col-4 text-center border-right text-bold" >6.000,00 TL</td>
+                    <td class="col-4 text-center border-right text-bold" >900,00 TL</td>
+                    <td class="col-4 text-center border-right text-bold" >5.100,00 TL</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<body>
+</head>
+</html>
+
+';
+
 echo $html;
 
 use Dompdf\Dompdf;
@@ -35,7 +172,7 @@ $dompdf->loadHtml($html);
 
 $document = 'bordro.pdf';
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$dompdf->setPaper('A4', 'P');
 
 // PDF'yi oluştur
 $dompdf->render();
@@ -43,7 +180,7 @@ ob_end_clean();
 
 
 //Dosyayı indir
-$dompdf->stream($document , array("Attachment" => false));
+$dompdf->stream($document, array("Attachment" => false));
 
 
 
